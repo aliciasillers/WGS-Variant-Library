83084b11b2ff80c209aba7ca267f09d04ca55041
asillers <asillers@ucdavis.edu>
2025-03-03 13:29:24 -0800

    file name change

--- fasterq.sh
+++ fasterq.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #
-#SBATCH --job-name=fastq
+#SBATCH --job-name=fasterq
 #SBATCH --ntasks=10 # Number of cores
 #SBATCH --nodes=1 # Ensure that all cores are on one machine
-#SBATCH --mem=120G # Memory pool for all cores in MB (see also --mem-per-cpu)
+#SBATCH --mem=100G # Memory pool for all cores in MB (see also --mem-per-cpu)
 #SBATCH --partition=bmh # Partition to submit to
-#SBATCH --output=fastq.out # File to which STDOUT will be written
-#SBATCH --error=fastq.err # File to which STDERR will be written
+#SBATCH --output=fasterq.out # File to which STDOUT will be written
+#SBATCH --error=fasterq.err # File to which STDERR will be written
 #SBATCH --mail-type=END,FAIL # Type of email notification- BEGIN,END,FAIL,ALL
 #SBATCH --mail-user=asillers@ucdavis.edu # Email to which notifications will be$
-#SBATCH --time=5:00:00
+#SBATCH --time=4:00:00
 #SBATCH --array=1-10
 
-sra=$(cat sralist1.txt | sed -n ${SLURM_ARRAY_TASK_ID}p)
+sra=$(cat sralist2.txt | sed -n ${SLURM_ARRAY_TASK_ID}p)
 
 module load sratoolkit
 
