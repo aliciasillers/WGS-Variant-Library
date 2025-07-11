c2edbe3bb511d13f8953e801c153fd4b81bb876d
asillers <asillers@ucdavis.edu>
2025-07-11 15:40:40 -0700

    script update for new data

--- bwaalign.sh
+++ bwaalign.sh
@@ -10,13 +10,13 @@
 #SBATCH --mail-type=END,FAIL # Type of email notification- BEGIN,END,FAIL,ALL
 #SBATCH --mail-user=asillers@ucdavis.edu # Email to which notifications will be$
 #SBATCH --time=1-00:00:00
-#SBATCH --array=1-10
+#SBATCH --array=1-168
 
 module load bwa
 module load samtools
 
-reads1=$(ls *1.fastq.gz | sed -n ${SLURM_ARRAY_TASK_ID}p)
-reads2=$(ls *2.fastq.gz | sed -n ${SLURM_ARRAY_TASK_ID}p)
-prefix=$(ls *1.fastq.gz | sed -n ${SLURM_ARRAY_TASK_ID}p | awk -F'[_]' '{print $1}')
+reads1=$(ls /quobyte/feldmanngrp/BACKED-UP/RawData/HudsonAlpha/20250421/Illumina_Resequencing/*R1.fastq.bz2 | sed -n ${SLURM_ARRAY_TASK_ID}p)
+reads2=$(ls /quobyte/feldmanngrp/BACKED-UP/RawData/HudsonAlpha/20250421/Illumina_Resequencing/*R2.fastq.bz2 | sed -n ${SLURM_ARRAY_TASK_ID}p)
+prefix=$(ls /quobyte/feldmanngrp/BACKED-UP/RawData/HudsonAlpha/20250421/Illumina_Resequencing/*R1.fastq.bz2 | sed -n ${SLURM_ARRAY_TASK_ID}p | awk -F'[._/]' '{print $13$14$18$21$22}')
 
-bwa mem -t 8 -R "@RG\tID:${prefix}\tSM:${prefix}" ../Genome/farr1 $reads1 $reads2 | samtools sort -@8 -o Mapped/$prefix.bwa.bam -
+bwa mem -t 8 -R "@RG\tID:${prefix}\tSM:${prefix}" ../Genome/farr1 <(bunzip2 -c $reads1) <(bunzip2 -c $reads2) | samtools sort -@8 -o Mapped/$prefix.bwa.bam -
