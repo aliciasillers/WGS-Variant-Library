b7f5ac83abd69fef345a4eca217984df2a57e35f
asillers <asillers@ucdavis.edu>
2025-10-17 13:31:06 -0700

    script updates

--- bbmapalign.sh
+++ bbmapalign.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
 #
 #SBATCH --job-name=bbmapalign
-#SBATCH --ntasks=70 # Number of cores
+#SBATCH --ntasks=40 # Number of cores
 #SBATCH --nodes=1 # Ensure that all cores are on one machine
-#SBATCH --mem=30G # Memory pool for all cores in MB (see also --mem-per-cpu)
+#SBATCH --mem=80G # Memory pool for all cores in MB (see also --mem-per-cpu)
 #SBATCH --partition=bmh # Partition to submit to
 #SBATCH --output=bbmapalign.out # File to which STDOUT will be written
 #SBATCH --error=bbmapalign.err # File to which STDERR will be written
 #SBATCH --mail-type=END,FAIL # Type of email notification- BEGIN,END,FAIL,ALL
 #SBATCH --mail-user=asillers@ucdavis.edu # Email to which notifications will be$
 #SBATCH --time=2-00:00:00
-#SBATCH --array=1-42
+#SBATCH --array=1-7
 
 module load conda
 conda activate wgs
 module load bbmap
+module load samtools
 
-L1R1=$(ls /quobyte/feldmanngrp/BACKED-UP/RawData/HudsonAlpha/20250421/Illumina_Resequencing/*L1_R1.fastq.bz2 | sed -n ${SLURM_ARRAY_TASK_ID}p)
-L1R2=$(ls /quobyte/feldmanngrp/BACKED-UP/RawData/HudsonAlpha/20250421/Illumina_Resequencing/*L1_R2.fastq.bz2 | sed -n ${SLURM_ARRAY_TASK_ID}p)
-L2R1=$(ls /quobyte/feldmanngrp/BACKED-UP/RawData/HudsonAlpha/20250421/Illumina_Resequencing/*L2_R1.fastq.bz2 | sed -n ${SLURM_ARRAY_TASK_ID}p)
-L2R2=$(ls /quobyte/feldmanngrp/BACKED-UP/RawData/HudsonAlpha/20250421/Illumina_Resequencing/*L2_R2.fastq.bz2 | sed -n ${SLURM_ARRAY_TASK_ID}p)
-prefix=$(ls /quobyte/feldmanngrp/BACKED-UP/RawData/HudsonAlpha/20250421/Illumina_Resequencing/*L1_R1.fastq.bz2 | sed -n ${SLURM_ARRAY_TASK_ID}p | awk -F'[._/]' '{print $12$13$14$15$16}')
+in1=$(ls *R1.fastq | sed -n ${SLURM_ARRAY_TASK_ID}p)
+in2=$(ls *R2.fastq | sed -n ${SLURM_ARRAY_TASK_ID}p)
+prefix=$(ls *R1.fastq | sed -n ${SLURM_ARRAY_TASK_ID}p | awk -F'[._/]' '{print $1}')
 
-In1=$(cat <(bunzip2 -c $L1R1) <(bunzip2 -c $L2R1))
-In2=$(cat <(bunzip2 -c $L1R2) <(bunzip2 -c $L2R2))
-
-bbmap.sh in1=$In1 in2=$In2 out=Mapped/$prefix.bb1.bam ref=../Genome/farr1.fa nodisk
+bbmap.sh in1=$in1 in2=$in2 out=Mapped/$prefix.bb1.bam ref=../Genome/farr1.fa nodisk
--- bwaalign.sh
+++ bwaalign.sh
@@ -10,15 +10,13 @@
 #SBATCH --mail-type=END,FAIL # Type of email notification- BEGIN,END,FAIL,ALL
 #SBATCH --mail-user=asillers@ucdavis.edu # Email to which notifications will be$
 #SBATCH --time=1-00:00:00
-#SBATCH --array=1-42
+#SBATCH --array=1-6
 
 module load bwa
 module load samtools
 
-L1R1=$(ls /quobyte/feldmanngrp/BACKED-UP/RawData/HudsonAlpha/20250421/Illumina_Resequencing/*L1_R1.fastq.bz2 | sed -n ${SLURM_ARRAY_TASK_ID}p)
-L1R2=$(ls /quobyte/feldmanngrp/BACKED-UP/RawData/HudsonAlpha/20250421/Illumina_Resequencing/*L1_R2.fastq.bz2 | sed -n ${SLURM_ARRAY_TASK_ID}p)
-L2R1=$(ls /quobyte/feldmanngrp/BACKED-UP/RawData/HudsonAlpha/20250421/Illumina_Resequencing/*L2_R1.fastq.bz2 | sed -n ${SLURM_ARRAY_TASK_ID}p)
-L2R2=$(ls /quobyte/feldmanngrp/BACKED-UP/RawData/HudsonAlpha/20250421/Illumina_Resequencing/*L2_R2.fastq.bz2 | sed -n ${SLURM_ARRAY_TASK_ID}p)
-prefix=$(ls /quobyte/feldmanngrp/BACKED-UP/RawData/HudsonAlpha/20250421/Illumina_Resequencing/*L1_R1.fastq.bz2 | sed -n ${SLURM_ARRAY_TASK_ID}p | awk -F'[._/]' '{print $12$13$14$15$16}')
+in1=$(ls *R1.fastq | sed -n ${SLURM_ARRAY_TASK_ID}p)
+in2=$(ls *R2.fastq | sed -n ${SLURM_ARRAY_TASK_ID}p)
+prefix=$(ls *R1.fastq | sed -n ${SLURM_ARRAY_TASK_ID}p | awk -F'[._/]' '{print $1}')
 
-bwa mem -t 8 -R "@RG\tID:${prefix}\tSM:${prefix}" ../Genome/farr1 <(cat <(bunzip2 -c $L1R1) <(bunzip2 -c $L2R1)) <(cat <(bunzip2 -c $L1R2) <(bunzip2 -c $L2R2)) | samtools sort -@8 -o Mapped/$prefix.bwa.bam -
+bwa mem -t 8 -R "@RG\tID:${prefix}\tSM:${prefix}" ../Genome/farr1 $in1 $in2 | samtools sort -@8 -o Mapped/$prefix.bwa.bam -
